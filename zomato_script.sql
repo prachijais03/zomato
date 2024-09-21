9d6f4396402edfcbb0de0c9501c45335224ee380
prachijais03 <prachijais03@gmail.com>
2024-09-21 21:30:53 +0530

    work for 21th sept'2024

--- zomato_script.sql
+++ zomato_script.sql
@@ -1,9 +1,7 @@
 /*
 	CREATE A DATABASE SCHEMA FOR ZOMATO
 */
-SHOW DATABASES;
-CREATE DATABASE zomato;
-USE zomato;
+
 
 /*
 
@@ -17,6 +15,8 @@ CREATE TABLE customer_profile (
     cemail varchar(100) NOT NULL,
     cgender enum('MALE', 'FEMALE', 'DO NOT WANT TO SPECIFY') NOT NULL
 ) AUTO_INCREMENT=10000;
+
+
 
 Alter Table Customer_profile Drop column cname;
 Alter Table Customer_profile Drop column cdob;
@@ -62,17 +62,7 @@ CREATE TABLE owner_profile (
 );
 */
 
-
-create table address (
-	addressId int primary key auto_increment,
-    line1 varchar(100),
-    line2 varchar(100),
-    line3 varchar(100),
-    city varchar(20),
-    state varchar(20),
-    pincode varchar(6)
-) auto_increment=100;
-
+/*
 create table profile (
 	pid int auto_increment primary key,
 	pname varchar(50) NOT NULL,
@@ -82,6 +72,7 @@ create table profile (
     pgender enum('MALE', 'FEMALE', 'DO NOT WANT TO SPECIFY') NOT NULL,
     ptype enum('CUSTOMER', 'OWNER', 'DELIVERY BOY') NOT NULL
 ) auto_increment=10000;
+drop table profile;
 
 describe profile;
 alter table profile add column addressId int;
@@ -112,6 +103,130 @@ VALUES ('Mike Johnson', '1995-07-12', 4444444444, 'mike.johnson@example.com', 'M
 
 
 select * from profile;
+*/
+
+
+/*
+Create Table user_profile (
+user_id INT auto_increment PRIMARY KEY,
+user_name varchar(100),
+user_dob date NOT NULL,
+user_emailid varchar(100) NOT NULL,
+user_contactno decimal NOT NULL,
+user_gender enum('male', 'female', 'DO NOT WANT TO SPECIFY') NOT NULL,
+user_type enum ('customer_profile', 'Business_profile') NOT NULL
+) auto_increment=1000;
+*/
+show tables;
+desc user_profile;
+drop table user_profile;
+/*
+create table messages(id int primary key auto_increment, message varchar(10));
+desc messages;
+insert into messages(message) value ("Hi");
+insert into messages(message) value ("How");
+insert into messages(message) value ("Are");
+insert into messages(message) value ("you?");
+select * from messages;
+drop table messages;
+
+-- start transaction;
+set autocommit=0;
+SET SQL_SAFE_UPDATES = 0;
+update messages set message="duggu";
+commit;
+
+rollback;
+
+show tables;
+*/
+
+SHOW DATABASES;
+CREATE DATABASE zomato;
+USE zomato;
+
+create table addressZ (
+	addressId int primary key auto_increment,
+    line1 varchar(100),
+    line2 varchar(100),
+    line3 varchar(100),
+    city varchar(20),
+    state varchar(20),
+    pincode varchar(6)
+) auto_increment=100;
+
+create table user_profile(
+user_id int primary key auto_increment,
+user_name varchar(100),
+user_dob date,
+user_emailid varchar(100),
+user_type enum('customer','bussiness') not null,
+user_addressid int,
+foreign key (user_addressid) references address (addressid)
+) auto_increment =1000;
+desc user_profile;
+
+create table user_address(
+user_contactno int primary key auto_increment,
+user_homeaddress varchar(100),
+user_officeaddress varchar(100),
+user_otheraddress varchar(100),
+useraddressid int,
+foreign key (useraddressid) references addressZ (addressId)
+) auto_increment =1000;
+desc user_address;
+
+create table payment(
+payment_id int primary key auto_increment,
+payment_type enum('cod','card','upi','net banking','wallet','zomato money') not null,
+payment_transactionid int,
+pament_transaction_time date,
+payment_transaction_status enum('failed','hold','successful') not null,
+payment_user_name varchar(100) ,
+payment_userid int,
+-- foreign key (payment_user_name) references user_profile (user_name),
+foreign key (payment_userid) references user_profile (user_id)
+) auto_increment =1000;
+desc payment;
+
+create table resturant(
+resturant_id int primary key auto_increment,
+resturant_name varchar(100),
+resturant_address varchar(100),
+resturant_email_id varchar(100),
+resturant_openingdate date,
+resturant_userid int,
+foreign key (resturant_userid) references user_profile(user_id)
+) auto_increment =1000;
+
+create table review(
+review_customerid int primary key auto_increment,
+review_resturant enum('outstanding','Good','Average','poor'),
+review_resturantid int,
+review_orderid int,
+foreign key (review_customerid) references user_profile(user_id),
+foreign key (review_resturantid ) references resturant(resturant_id)
+) auto_increment=1000;
+
+create table orders(
+order_id int primary key auto_increment,
+order_itemname varchar(100),
+order_itemquantity int,
+order_itemprice int,
+order_address varchar(100),
+order_addressid int,
+order_resturantid int,
+order_reviewid int,
+order_paymentid int,
+order_userid int,
+foreign key (order_addressid) references addressZ(addressid),
+foreign key (order_resturantid) references resturant(resturant_id),
+foreign key (order_reviewid) references review(review_customerid),
+foreign key (order_paymentid) references payment(payment_id),
+foreign key (order_userid) references user_profile(user_id)
+) auto_increment =1000;
+
+
 
 
 
